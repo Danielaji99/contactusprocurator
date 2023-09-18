97a9584ae4f9f419c794db4af120bfc55a6b9914
Daniel Aji <danielenochaji@gmail.com>
2023-09-18 12:11:39 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+
+
+const details = document.querySelector(".contact-display");
+
+const arr = [];
+
+
+  
+
+    function add() {
+        let name = (document.getElementById("name").value);
+        let number = (document.getElementById("number").value);
+          arr.push({name:name , number : number});
+            details.innerHTML = ""
+            for(
+                let i = 0; i < arr.length ; i++
+                
+            ){
+            const innerElem = `<div>
+            <p>Name:${arr[i].name}</p>
+            <p>Number:${arr[i].number}</p>
+            </div>`;
+            details.innerHTML += innerElem;
+            } 
+        
+         }
